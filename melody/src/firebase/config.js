07b725396ad30801f92a3492309d70b1200eaf33
andrewondara <a.ondara14@gmail.com>
2025-01-22 14:41:48 -0600

    Hide env

--- melody/src/firebase/config.js
+++ melody/src/firebase/config.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBGocxgnQ9vciCS5KDa8tudeGELcvodFO8",
-  authDomain: "melody-dda44.firebaseapp.com",
-  projectId: "melody-dda44",
-  storageBucket: "melody-dda44.firebasestorage.app",
-  messagingSenderId: "1077550231222",
-  appId: "1:1077550231222:web:7dc0bee8886db50b7b8017",
-  measurementId: "G-YWFHK174VD"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
